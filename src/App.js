6976889de59b1c0851c35178bcceb4390a843c77
Mahmoud-Elhassan <bp.mahmoud7@gmail.com>
2021-12-30 18:36:01 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./app.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [activity, setActivity] = useState("");
+
+    useEffect(() => {
+        axios
+            .get("http://www.boredapi.com/api/activity/")
+            .then((res) => {
+                setActivity(res.data.activity);
+            })
+            .catch((error) => console.log(error));
+    }, []);
+
+    return (
+        <div className="App">
+            <div id="activity">
+                {activity.length !== 0 ? activity : "LOADING..."}
+            </div>
+        </div>
+    );
 }
 
 export default App;
